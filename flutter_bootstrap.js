0159259e76541af7f27694a951e6c6ce49b4153d
meltzow <meltzow@users.noreply.github.com>
2024-08-24 20:41:17 +0000

    deploy: meltzow/metrix@d2cc6331fda1e01758b4b90e5897b294e79537de

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3650724301"
+    serviceWorkerVersion: "1084063060"
   }
 });
